a675b0e8426c924befc2ddfafe220ab9bd5a5b79
Artem Andreev <43151230+CeramicThree@users.noreply.github.com>
2021-04-07 15:33:28 +0300

    AddPost Form ver.1

--- forum_frontend/src/components/Post/AddPost.js
+++ forum_frontend/src/components/Post/AddPost.js
@@ -3,8 +3,37 @@ import { FormGroup, Label, Input, Button, Form} from 'reactstrap';
 import AppNav from '../AppNav/AppNav';
 
 class AddPost extends Component{
-    async handleSubmit(){
+    constructor(props){
+        super(props);
+        this.state = {
+            header: '',
+            text: '',
+            date: '',
+            author: {
+                id: ''
+            }
+        }
         
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    async handleSubmit(event){
+        this.setState({author: {id:'4'}})
+        await fetch('/api/posts/', {
+            method: 'POST',
+            body: JSON.stringify(this.state)
+        }.then(function(response) {
+        console.log(response)
+        return response.json();
+        });
+                    
+        event.preventDefault();      
+    }
+    
+    
+    handleChange(event) {
+        this.setState({event.target.name : event.target.value});  
     }
 
     render(){
@@ -16,11 +45,11 @@ class AddPost extends Component{
                     <Form onSubmit = {this.handleSubmit}>
                         <FormGroup>
                             <Label tag="h5" for = "header">Header</Label>
-                            <Input type = "text" name = "header" id = "header"/>
+                            <Input type = "text" name = "header" id = "header" onChange = {this.handleChange}/>
                         </FormGroup>
                         <FormGroup>
                             <Label tag="h5" for = "text">Text</Label>
-                            <Input type = "textarea" name = "text" id = "text" style={{height : "25vh"}}/>
+                            <Input type = "textarea" name = "text" id = "text" style={{height : "25vh"}} onChange = {this.handleChange}/>
                         </FormGroup>
                         <Button type="submit" className="btn-lg text-center" color="success">Add</Button>
                     </Form>
